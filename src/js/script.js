6cf118cebc4bc61ded9eb1da627bca1d26d01f69
YuriiNik <yura2203@gmail.com>
2022-03-21 23:56:43 +0200

    add script and adaptation

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+var name="Ivan";
+let number=7;
+const pi=3.14;
+
+number=4;
+// pi=5;
+
+let leftBorderWidth=200;
+
+// number
+// string - "", '', ``
+// true/false
+// null
+// underfined
+// let obj = {
+//     name: 'apple',
+//     color: 'green'
+//     weight: 200
+// }
+// symbol
+
+// alert(1234)
+// console.log(number);
+// let answer=confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer=prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// console.log(4+'df')
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
