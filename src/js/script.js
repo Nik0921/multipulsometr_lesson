5eea93096de813f11af3d035f9e1cc5c409dc39a
YuriiNik <yura2203@gmail.com>
2022-04-03 18:14:09 +0300

    add openServer and scrollFunction

--- src/js/script.js
+++ src/js/script.js
@@ -110,7 +110,59 @@ function toggleSlide(item) {
   valideForms('#consultation form');
   valideForms('#order form');
 
+  $('input[name=phone]').mask("+3 (999) 999-9999");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    if (!$(this).valid()) {
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+    });
+    return false;
   });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+
+};
+});
+});
 
 //  const slider = tns({
 //     container: '.carousel__inner',
